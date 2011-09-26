bc2b46af90fb99426ef0272daff141f2cae8ab9a
Michael Stahnke <stahnma@puppetlabs.com>
2011-09-25 19:22:15 -0700

    Explain that master branch isn't used for much.

--- spec/unit/indirector/file_metadata/rest_spec.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env rspec
-require 'spec_helper'
-
-require 'puppet/indirector/file_metadata'
-
-describe "Puppet::Indirector::Metadata::Rest" do
-  it "should add the node's cert name to the arguments"
-end
